e4b10b17dc4580f13fb809d0bdd35d4d8b9857cb
rraju007 <rraju007@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-16 10:49:37 +0000

    completed
    git-svn-id: https://plugins.svn.wordpress.org/nofollow-doctor-wp/trunk@1091221 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,27 @@
+=== WordPress Nofollow Master ===
+Contributors: rraju007
+Donate link: 
+Tags: noffolw-auto,auto-newtab,detect-noffow,auto-noffow-wp-newtab
+Requires at least: 3.3
+Tested up to:4.1
+Stable tag: 1.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This plug-in detect external links automatically and makes them no-follow , also open them in new tab.Simply enjoy
+
+== Description ==
+
+This plug-in detect external links automatically and makes them no-follow , also open them in new tab.Simply enjoy
+
+== Installation ==
+
+1.Download this plgun.
+2.Upload it by wordpress dashboard as usual.
+3.And finally active it , thats it
+    Enjoy
+
+
+== Frequently asked questions ==
+
+== Screenshots ==
---
+++ wp-noffow-master.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Plugin Name: WordPress No-follow Master
+Plugin URI: 
+Description: This plug-in detect external links automatically and makes them no-follow , also open them in new tab. Its created by <strong>OMAR FARUK</strong>.Simply enjoy.
+Author:Omar F 
+Author URI:https://twitter.com/rraju007
+Version: 1.0
+*/
+/*Some Set-up*/
+define('EXTERNAL_LINKS_DETECTOR', WP_PLUGIN_URL . '/' . plugin_basename( dirname(__FILE__) ) . '/' );
+/* Adding Latest jQuery from Wordpress */
+function jlatest_wp_nofollow_master() {
+	wp_enqueue_script('jquery');
+}
+add_action('init', 'jlatest_wp_nofollow_master');
+/* Adding plugin javascript active file */
+wp_enqueue_script('active-js', EXTERNAL_LINKS_DETECTOR.'js.js', array('jquery'), '1.0', true);
+
+function noFollowReplace($buffer){ // $buffer contains entire page
+	$currentSite = site_url();
+	$find = '/( href=")(?!(' . str_replace('/', '\/', $currentSite) . ')|\#|\/)/i';
+	$replace = ' rel="nofollow" href="$2';
+	$buffer = preg_replace($find, $replace, $buffer);
+	return $buffer;
+}
+
+function noFollowAll(){
+	ob_start();
+	ob_start('noFollowReplace');
+}
+add_action('template_redirect', 'wp_nofollow_master');
+
+
+
