32b469cc2ca346d07325a3283094525837057e8b
rraju007 <rraju007@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-08 13:41:44 +0000

    Vesion 1.1 updated and all problem fixed for new version of wp
    git-svn-id: https://plugins.svn.wordpress.org/nofollow-doctor-wp/trunk@1108093 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-noffow-master.php
+++ wp-noffow-master.php
@@ -5,31 +5,27 @@ Plugin URI: https://wordpress.org/plugins/nofollow-doctor-wp/
 Description: This plug-in detect external links automatically and makes them no-follow , also open them in new tab. Its created by <strong>OMAR FARUK</strong>.Simply enjoy.
 Author:Omar F 
 Author URI:https://twitter.com/rraju007
-Version: 1.0
+Version: 1.1
 */
 /*Some Set-up*/
-define('EXTERNAL_LINKS_DETECTOR', WP_PLUGIN_URL . '/' . plugin_basename( dirname(__FILE__) ) . '/' );
-/* Adding Latest jQuery from Wordpress */
-function jlatest_wp_nofollow_master() {
-	wp_enqueue_script('jquery');
-}
-add_action('init', 'jlatest_wp_nofollow_master');
-/* Adding plugin javascript active file */
-wp_enqueue_script('active-js', EXTERNAL_LINKS_DETECTOR.'js.js', array('jquery'), '1.0', true);
 
-function noFollowReplace($buffer){ // $buffer contains entire page
-	$currentSite = site_url();
-	$find = '/( href=")(?!(' . str_replace('/', '\/', $currentSite) . ')|\#|\/)/i';
-	$replace = ' rel="nofollow" href="$2';
-	$buffer = preg_replace($find, $replace, $buffer);
-	return $buffer;
+/**
+* add nofollow to links
+*/
+$my_domain = get_bloginfo('wpurl');
+function add_nofollow_content($content) {
+$content = preg_replace_callback(
+'/<a[^>]*href=["|\']([^"|\']*)["|\'][^>]*>([^<]*)<\/a>/i',
+function($m) {
+if (strpos($m[1], $my_domain) === false)
+return '<a href="'.$m[1].'" rel="nofollow" target="_blank">'.$m[2].'</a>';
+else
+return '<a href="'.$m[1].'" target="_blank">'.$m[2].'</a>';
+},
+$content);
+return $content;
 }
-
-function noFollowAll(){
-	ob_start();
-	ob_start('noFollowReplace');
-}
-add_action('template_redirect', 'wp_nofollow_master');
+add_filter('the_content', 'add_nofollow_content');
 
 
 
